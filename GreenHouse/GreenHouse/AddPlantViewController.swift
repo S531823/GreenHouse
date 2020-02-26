1fd42d04ff7f30453e752da6d4de5fe3115d59d3
Sam Fields <s526635@nwmissouri.edu>
2020-02-25 19:21:26 -0600

    Created AddPlantViewController for adding plants into the app.

---
+++ GreenHouse/GreenHouse/AddPlantViewController.swift
@@ -0,0 +1,27 @@
+//
+//  AddPlantViewController.swift
+//  GreenHouse
+//
+//  Created by student on 2/25/20.
+//  Copyright © 2020 S531823. All rights reserved.
+//
+
+import UIKit
+
+class AddPlantViewController: UIViewController {
+
+    @IBOutlet weak var nameTF:UITextField!
+    @IBOutlet weak var speciesTF:UITextField!
+    @IBOutlet weak var waterTimesTF:UITextField!
+    @IBOutlet weak var waterPeriodTF:UITextField!
+    @IBOutlet weak var sunlightTimesTF:UITextField!
+    @IBOutlet weak var sunlightPeriodTF:UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+
+}
