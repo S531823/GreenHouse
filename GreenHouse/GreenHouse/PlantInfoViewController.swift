705117d6adea018dfcd95e335c026293fd97f34e
Student <revanth.d1995@gmail.com>
2020-02-28 21:41:25 -0600

    added plant info

---
+++ GreenHouse/GreenHouse/PlantInfoViewController.swift
@@ -0,0 +1,55 @@
+//
+//  PlantInfoViewController.swift
+//  GreenHouse
+//
+//  Created by Student on 2/28/20.
+//  Copyright © 2020 S531823. All rights reserved.
+//
+
+import UIKit
+
+class PlantInfoViewController: UIViewController {
+
+    
+    @IBOutlet weak var editNameTF: UITextField!
+    
+    
+    @IBOutlet weak var editSpeciesTF: UITextField!
+    
+    
+    @IBOutlet weak var editWaterFrequencyTF: UITextField!
+    
+    
+    @IBOutlet weak var editWaterTimes: UITextField!
+    
+    
+    @IBOutlet weak var editSunlightExposureTF: UITextField!
+    
+    
+    @IBOutlet weak var editSunlightExposureHoursTF: UITextField!
+    
+    
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        navigationItem.title = "Plants name"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Cancel", style: .plain, target: nil, action: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
